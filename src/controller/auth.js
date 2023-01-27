0c6bd72315fff5a9963f9930d667f1050e936c44
ChristianVarela <christian_varela@outlook.com.br>
2023-01-26 23:39:45 -0300

    feat: finalizado login completo pela API

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,17 +1,29 @@
 import { usersCollection } from "../config/database.js"
 import bcrypt from 'bcrypt'
+import {v4 as uuid} from 'uuid'
 
 export const signIn = async (req, res) => {
     const { email, password } = req.body
     const user = await usersCollection.findOne({email})
     if (!user){
-        return res.status(401).send("Usuario não cadastrado!")
+        return res.status(401).send("Usuário não cadastrado!")
     }
     const passwordCompare = bcrypt.compareSync(password, user.password)
     if (!passwordCompare){
         return res.status(401).send("Senha está incorreta!")
-    } else {
-        return res.send()
-    }
+    } 
+    const token = uuid()
+    return res.send({token, user_id: user._id})
 
+}
+
+export const signUp = async (req, res) => {
+    const {name, email, password, confirmPassword} = req.body
+    const user = await usersCollection.findOne({email})
+    if (user){
+        return res.status(400).send("Email já esta sendo usado!")
+    }
+    const passwordCrypted = bcrypt.hashSync(password, 10)
+    await usersCollection.insertOne({name , email, password: passwordCrypted})
+    return res.status(201).send("Usuário criado com sucesso!")
 }
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,8 +1,18 @@
-import { signInSchema } from "../schema/auth.js"
+import { signInSchema, signUpSchema } from "../schema/auth.js"
 
 export const signInValidate = (req, res, next) => {
     const { email, password } = req.body
     const validation = signInSchema.validate({email, password})
+    if (validation.error){
+        const errors = validation.error.details.map((e) => e.message)
+        return res.status(422).send(errors)
+    }
+    next()
+}
+
+export const signUpValidate = (req, res, next) => {
+    const { name, email, password, confirmPassword } = req.body
+    const validation = signUpSchema.validate({name, email, password, confirmPassword})
     if (validation.error){
         const errors = validation.error.details.map((e) => e.message)
         return res.status(422).send(errors)
